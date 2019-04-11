13f094d6a18059fb59b1998c2921951addb11281
Simen Skrede Laane <010096@usn.no>
2019-04-11 11:21:04 +0200

    Userpage

--- APP2000V-DesktopApp-g11/Views/UserPage.xaml.cs
+++ APP2000V-DesktopApp-g11/Views/UserPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using APP2000V_DesktopApp_g11.Assets;
+using APP2000V_DesktopApp_g11.Controllers;
+using APP2000V_DesktopApp_g11.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +21,56 @@ namespace APP2000V_DesktopApp_g11.Views
     /// <summary>
     /// Interaction logic for UserPage.xaml
     /// </summary>
-    public partial class UserPage : Page
+    public partial class UserPage : AnimatedUserControl
     {
         public UserPage()
         {
             InitializeComponent();
+        }
+
+        public double WIDTH_COLUMN_SEPERATOR { get; private set; }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            List<UIElement> visibleItems = new List<UIElement>();
+            double xColumn1 = 0;
+            double xColumn2 = (finalSize.Width / 2) + (WIDTH_COLUMN_SEPERATOR / 2);
+            double y = 0;
+
+            double columnWidth = (finalSize.Width - WIDTH_COLUMN_SEPERATOR) / 2;
+
+            for (int i = 0; i < InternalChildren.Count; i++)
+            {
+                UIElement child = InternalChildren[i];
+                if (child.Visibility != Visibility.Collapsed)
+                {
+                    visibleItems.Add(child);
+                }
+            }
+
+            for (int i = 0; i < visibleItems.Count; i++)
+            {
+                if (i >= (visibleItems.Count - 1))
+                {
+                    visibleItems[i].Arrange(new Rect(xColumn1, y, columnWidth, visibleItems[i].DesiredSize.Height));
+                }
+                else
+                {
+                    UIElement leftItem = visibleItems[i];
+                    UIElement rightItem = visibleItems[i + 1];
+
+                    double rowHeight = leftItem.DesiredSize.Height > rightItem.DesiredSize.Height ? leftItem.DesiredSize.Height : rightItem.DesiredSize.Height;
+
+                    leftItem.Arrange(new Rect(xColumn1, y, columnWidth, rowHeight));
+                    rightItem.Arrange(new Rect(xColumn2, y, columnWidth, rowHeight));
+
+                    y += rowHeight;
+
+                    i++;
+                }
+            }
+
+            return finalSize;
         }
     }
 }
